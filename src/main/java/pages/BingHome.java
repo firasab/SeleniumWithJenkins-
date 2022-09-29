e280dbd86fb1ad1e501c234c784b45090ad3c34a
firas <firas.abu.sneneh@hotmail.com>
2022-09-29 11:21:03 +0300

    add files

---
+++ src/main/java/pages/BingHome.java
@@ -0,0 +1,23 @@
+package pages;
+
+
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+
+public class BingHome {
+    WebDriver driver;
+    WebElement searchTxtBox;
+    WebElement searchBtn;
+
+    public BingHome(WebDriver driver) {
+        this.driver = driver;
+        this.searchBtn = driver.findElement(By.xpath("//*[@id=\"search_icon\"]"));
+        this.searchTxtBox = driver.findElement(By.name("q"));
+    }
+
+    public void search(String txt) {
+        this.searchTxtBox.sendKeys(txt);
+        this.searchBtn.click();
+    }
+}
